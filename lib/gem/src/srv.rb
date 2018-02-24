3fad55eac03edf7c6bfc938c081da9f0b953ba99
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-02-24 21:45:07 +0900

    bunlde gem

---
+++ lib/gem/src/srv.rb
@@ -0,0 +1,9 @@
+require "gem/src/srv/version"
+
+module Gem
+  module Src
+    module Srv
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/gem/src/srv/version.rb
@@ -0,0 +1,7 @@
+module Gem
+  module Src
+    module Srv
+      VERSION = "0.1.0"
+    end
+  end
+end
