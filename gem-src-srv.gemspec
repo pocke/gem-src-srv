00ed133161d8f1c77d4ebbe5c1799545fc87cd7c
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2018-02-25 11:13:11 +0900

    Retry and spawn
    Close #1
    Close #2

--- gem-src-srv.gemspec
+++ gem-src-srv.gemspec
@@ -21,4 +21,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.16"
   spec.add_development_dependency "rake", "~> 10.0"
+
+  spec.add_runtime_dependency "retryable", "~> 3.0"
 end
--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -1,10 +1,16 @@
 require 'net/http'
 require 'uri'
+require 'retryable'
 
 Gem.post_install do |installer|
-  uri = URI.parse('http://localhost:20080/gem_install')
-  req = Net::HTTP::Post.new(uri)
-  req.body = Marshal.dump(installer.spec)
-  Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req) }
-rescue # FIXME
+  next if installer.spec.name == 'gem-src-srv'
+
+  spawn('gem-src-srv', [:out, :err] => '/dev/null')
+
+  Retryable.retryable(tries: 3, sleep: 0.1, on: Errno::ECONNREFUSED) do
+    uri = URI.parse('http://localhost:20080/gem_install')
+    req = Net::HTTP::Post.new(uri)
+    req.body = Marshal.dump(installer.spec)
+    Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req) }
+  end
 end
